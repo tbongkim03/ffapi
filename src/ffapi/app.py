0fb2832c12726da7f73959ed234f717db46febd4
tbongkim03 <tbongkim03@gmial.com>
2024-08-13 12:30:59 +0900

    fill nation code

--- src/ffapi/app.py
+++ src/ffapi/app.py
@@ -4,17 +4,42 @@ from fastapi import FastAPI, HTTPException
 
 from fastapi.responses import FileResponse
 import pandas as pd
+import os
+import requests
 
 app = FastAPI()
 df = pd.read_parquet('/home/michael/code/ffapi/data')
 
-#@app.get("/")
-#def read_root():
-#    return {"Hello": "World"}
+key = os.getenv('MOVIE_API_KEY')
+base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json"
+
+#def get_key():
+#    key = os.getenv('MOVIE_API_KEY')
+#    return key
+
+def req(movie_cd):
+    url = f"{base_url}?key={key}&movieCd={movie_cd}"
+    r = requests.get(url)
+    code = r.status_code
+    data = r.json()
+    return data
+
+def req2nationCd(movie_cd):
+    data = req(movie_cd)
+    nationCds = []
+    nations = data.get('movieInfoResult').get('movieInfo').get('nations')
+    for nation in nations:
+        if nation.get('nationNm') == '한국':
+            nationCode = 'K'
+        else:
+            nationCode = 'F'
+        nationCds.append(nationCode)
+    return nationCds
 
 @app.get("/")
 def read_root():
-    return FileResponse('index.html')
+    #return FileResponse('index.html')
+    return "hello"
 
 @app.get('/sample/')
 def sample():
@@ -27,9 +52,14 @@ def movie_meta(movie_cd: str):
 
     # df에서 movieCd == movie_cd row를 조회
     # 조회된 데이터를 .to_dict()로 만들어 아래에서 return
-    sdf = df.sample(n=5)
-    
+   
     movie_cd_df = df[df['movieCd'] == movie_cd]
+    nationCodes = req2nationCd(movie_cd)
+
+    for key, value in movie_cd_df['repNationCd'].items():
+        if value is None:
+            movie_cd_df['repNationCd'][key] = ','.join(nationCodes)
+
     if movie_cd_df.empty:
         raise HTTPException(status_code=404, detail='영화를 찾을 수 없습니다')
     dict_movie_cd = movie_cd_df.iloc[0].to_dict()
@@ -37,3 +67,6 @@ def movie_meta(movie_cd: str):
 
     #return {"movie_cd": movie_cd, "df_count": len(df)}
     return dict_movie_cd
+
+
+
